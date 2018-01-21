043e6aad4991ae800298d649e466552148343cc5
bcsasankh <bcsasankh@gmail.com>
2018-01-21 14:35:18 -0800

    added build command

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
     './src/index.js'
@@ -16,6 +18,20 @@ module.exports = {
       }
     }]
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      ENV: JSON.stringify(process.env.ENV),
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ],
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
